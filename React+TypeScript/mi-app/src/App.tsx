b878d62ba55798bc6d249c1639d2d414b4cb2a8d
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2024-04-23 22:33:08 -0500

    Add React+TypeScript

---
+++ React+TypeScript/mi-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { Counter } from "./components/Counter";
+import { Usuario } from './components/Usuario';
+import { Efecto } from './components/Efecto';
+
+function App() {
+  return (
+    <>
+      <h1>React + TypeScript</h1>
+      <hr />
+      <Counter/>
+      <hr />
+      <Usuario/>
+      <hr />
+      <Efecto/>
+    </>
+  );
+}
+
+export default App;
